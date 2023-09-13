a3fee6aff1d7570669bb215ef84e70d8e4943f4f
Luis Herrera <leggetram@gmail.com>
2023-09-13 16:18:23 -0400

    agregando la app2, requiere una actualizacion de  DRY

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import imagen1 from "./images/image1.jpg";
 import imagen2 from "./images/image2.jpg";
 import imagen3 from "./images/image3.jpg";
 import { useRef } from "react";
+import { useState } from "react";
 import './App.css';
 
 function App() {
@@ -17,7 +18,6 @@ function App() {
       e.target.src=imagen3;
     }
   }
-
   const alterTxt=(e)=>{
     if(e.target.innerHTML === "visto"){
       e.target.innerHTML = "";
@@ -25,7 +25,6 @@ function App() {
       e.target.innerHTML = "visto";
     }
   }
-
   const reyesGodos = [
   { picture: <img onClick={alterImg} alt="stormTrooper" src={imagen0} /> }, { name: <span onClick={alterTxt}>storm trooper</span> },
   { picture: <img onClick={alterImg} alt="fox" src={imagen1} /> }, { name: <span onClick={alterTxt}>fooxi</span> },
@@ -33,11 +32,9 @@ function App() {
   ]; //used in "cards-container"
   /*script app0/////////////////////////////////////////////////*/
 
-
   /*script app1--------------------------------------------------*/
   const refElement = useRef(); //importamos el hook useRe de react
   const cambioBolivianos = 7.20;
-
   const aumentar = (e) => {
     if (e.target.value === "clíck aquí") {
       e.target.value = 0;
@@ -53,7 +50,6 @@ function App() {
       e.target.style.color = "black";
     }
   }
-
   const cambio = () => {/*esta funcion realiza el cambio de dolares en refElement a bolivianos y la regresa en  refElement*/
 
     if (refElement.current.value === "clíck aquí") {
@@ -62,7 +58,6 @@ function App() {
       refElement.current.value = Number(refElement.current.value) * cambioBolivianos;
     }
   }
-
   const cambioImagen = (e) => {
     if(e.target.src.includes("image0")){
       e.target.src=imagen1;
@@ -70,18 +65,88 @@ function App() {
       e.target.src=imagen0;
     }
   }
-
   const lectura=(e)=>{
     refElement.current.value = e.target.value;
   }
-
   /*script app1///////////////////////////////////////////////////*/
+
+  /*script app2----------------------------------------------------*/
+
+  //count
+  const [contador, setContador] = useState(0); //aqui inicializamos contador en 0
+  const disminuir = () =>{
+    setContador(contador-1);
+  }
+  const aumentarSecondApp =()=>{
+    setContador(contador+1);
+  }
+  const restablecer=()=>{
+    setContador(0);
+  }
+
+  //calculator
+  const[numero1, setNumero1] = useState(0);
+  const[numero2, setNumero2] = useState(0);
+  const[resultado, setResultado] = useState(0);
+
+  const modifSumator1 = (e) =>{
+    setNumero1(e.target.value);
+  }
+  const modifSumator2 = (e) =>{
+    setNumero2(e.target.value);
+  }
+  const sumar = () =>{
+    setResultado(Number(numero1) + Number(numero2)); //numero 1 es texto y numero 2 es texto, ambas variables se les debe aplicar el number por separado
+  }
+
+  //afisiones
+  const kingAfi = [
+    {nombre: <span className="king-nombre">Ataúlfo</span>, aficion: <span className="king-aficion">Comer toros sin pelar</span>},
+    {nombre: <span className="king-nombre">Teodorico</span>, aficion: <span className="king-aficion">Ver Documentales de la tele</span>},
+    {nombre: <span className="king-nombre">Recesvinto</span>, aficion: <span className="king-aficion">Leer a Hegel</span>}
+  ];
+  const [kingContador, setKingContador] = useState(0);
+  const [mensaje, setMensaje] = useState();
+  
+  const afiAlter = () =>{
+    if(kingContador+1 >= kingAfi.length){
+      setKingContador(0);
+    }else{
+      setKingContador(Number(kingContador)+1);
+    }
+    setMensaje(<p>La aficion de <span className="king-nombre">{kingAfi[kingContador].nombre}</span>
+    es <span className="king-aficion">{kingAfi[kingContador].aficion}</span></p>);
+  }
+
+  //cambiazo
+  const tasaCambios = [
+    {moneda: "Euro", cambio: 1},
+    {moneda: "Peso Argentino", cambio: 118.6},
+    {moneda: "Peso Colombiano", cambio: 4543.5},
+    {moneda: "Peso Mexicano", cambio:23.2},
+    {moneda: "Dólar", cambio: 1.14}
+  ];
+
+  const[cambiario, setCambiario] = useState(0);
+  const[res1, setRes1] = useState(0);
+  const[res2, setRes2] = useState(0);
+  const[res3, setRes3] = useState(0);
+  const[res4, setRes4] = useState(0);
+  
+  const cambiazo = (e) =>{
+    setCambiario(e.target.value);
+    setRes1(Number(e.target.value) * Number(tasaCambios[1].cambio));
+    setRes2(Number(e.target.value) * Number(tasaCambios[2].cambio));
+    setRes3(Number(e.target.value) * Number(tasaCambios[3].cambio));
+    setRes4(Number(e.target.value) * Number(tasaCambios[4].cambio));
+  }
+
+  /*script app2///////////////////////////////////////////////////////*/
 
   return (
     <div className="App">
 
-      <section className="cards-container">
-
+      <section className="container">
         <div className="card">
           {reyesGodos[0].picture}
           {reyesGodos[1].name}
@@ -96,15 +161,57 @@ function App() {
           {reyesGodos[4].picture}
           {reyesGodos[5].name}
         </div>
-
       </section>
 
-      <section className="app1-container">
+      <section className="container">
         <div className="app1">
           <input ref={refElement} type="text" onClick={aumentar} value="clíck aquí" /> {/*value ya que es un input, si fuera un div podríamos cambiar el innerHTML*/}
           <button onClick={cambio}>Aceptar</button>
           <div className="card"><img onClick={cambioImagen} src={imagen0} /> </div>
           <input onChange={lectura} type="text" />
+        </div>
+
+      </section>
+
+      <section className="container">
+        <div className="app2">
+          
+          <div className="app2-count card">
+            <h1>{contador}</h1>
+            <button onClick={disminuir}>disminuir</button>
+            <button onClick={aumentarSecondApp}>aumentar</button>
+            <button onClick={restablecer}>restablecer</button>
+          </div>
+
+          <div className="app2-sumator card">
+            <input type="number" value={numero1} onChange={modifSumator1}/>
+            <input type="number" value={numero2} onChange={modifSumator2}/>
+            <input type="number" value={resultado} readOnly/>
+            <button onClick={sumar}>Sumar</button>
+          </div>
+
+          <div className="app2-king-afi card">
+            <button onClick={afiAlter}>Ver siguiente</button>
+            <div> {mensaje} </div>
+          </div>
+
+          <div className="app2-cambiazo card">
+            <label htmlFor="u0">{tasaCambios[0].moneda}</label>
+            <input type="number" value={cambiario} onChange={cambiazo} id="u0"/>
+
+            <label htmlFor="u1">{tasaCambios[1].moneda}</label>
+            <input type="number" value={res1} readOnly id="u1"/>
+
+            <label htmlFor="u2">{tasaCambios[2].moneda}</label>
+            <input type="number" value={res2} readOnly id="u2"/>
+
+            <label htmlFor="u3">{tasaCambios[3].moneda}</label>
+            <input type="number" value={res3} readOnly id="u3"/>
+
+            <label htmlFor="u4">{tasaCambios[4].moneda}</label>
+            <input type="number" value={res4} readOnly id="u4"/>
+          </div>
+
         </div>
 
       </section>
