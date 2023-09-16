3df128cfae1677a0cb234825912b0a6e6c8a5410
Luis Herrera <leggetram@gmail.com>
2023-09-15 20:50:04 -0400

    completado-actividad-reyes

--- src/App.js
+++ src/App.js
@@ -4,47 +4,31 @@ import Cena from './Cena';
 import './App.css';
 
 function App() {
+  const reyes = [
+    {nombre: "Atanagildo", color:"darkolivegreen", precio:178},
+    {nombre: "Ervigio", color:"crimson", precio:169},
+    {nombre: "Ataúlfo", color:"darkred", precio:81},
+    {nombre: "Leogivildo", color:"darkmagenta", precio:126},
+    {nombre: "Recesvinto", color:"brown", precio:142},    
+    {nombre: "Sisebuto", color: "blue", precio: 69}
+  ];
 
-  const reyes=[
-    {
-      nombre:"Atanagildo",
-      color:"darkolivegreen",
-      precio:178
-    },{
-      nombre:"Ervigio",
-      color:"crimson",
-      precio:169
-    },{
-      nombre:"Ataúlfo",
-      color:"peru",
-      precio:81
-    },{
-      nombre:"Leogivildo",
-      color:"darkmagenta",
-      precio:126
-    },{
-      nombre:"Recesvinto",
-      color:"royalblue",
-      precio:141
-    },{
-      nombre:"Sisebuto",
-      color:"teal",
-      precio:69
-    }
-  ]
   const [total, setTotal] = useState(0);
-  return (
 
+  return (
     <>
-      <h1>Total a pagar: {total}$</h1>
-      <cena nombre={reyes[0].nombre} color={reyes[0].color} precio={reyes[0].precio} setTotal={setTotal} />
-      <cena nombre={reyes[1].nombre} color={reyes[1].color} precio={reyes[1].precio} setTotal={setTotal} />
-      <cena nombre={reyes[2].nombre} color={reyes[2].color} precio={reyes[2].precio} setTotal={setTotal} />
-      <cena nombre={reyes[3].nombre} color={reyes[3].color} precio={reyes[3].precio} setTotal={setTotal} />
-      <cena nombre={reyes[4].nombre} color={reyes[4].color} precio={reyes[4].precio} setTotal={setTotal} />
-      <cena nombre={reyes[5].nombre} color={reyes[5].color} precio={reyes[5].precio} setTotal={setTotal} />
+    <h1>Total a pagar: {total}</h1>
+    <div className='caja-central'>
+      <Cena nombre={reyes[0].nombre} color={reyes[0].color} precio={reyes[0].precio} setTotal={setTotal}></Cena>
+      <Cena nombre={reyes[1].nombre} color={reyes[1].color} precio={reyes[1].precio} setTotal={setTotal}></Cena>
+      <Cena nombre={reyes[2].nombre} color={reyes[2].color} precio={reyes[2].precio} setTotal={setTotal}></Cena>
+      <Cena nombre={reyes[3].nombre} color={reyes[3].color} precio={reyes[3].precio} setTotal={setTotal}></Cena>
+      <Cena nombre={reyes[4].nombre} color={reyes[4].color} precio={reyes[4].precio} setTotal={setTotal}></Cena>
+      <Cena nombre={reyes[5].nombre} color={reyes[5].color} precio={reyes[5].precio} setTotal={setTotal}></Cena>
+    </div>
     </>
+
   )
 }
 
-export default App;
+export default App
--- src/Cena.js
+++ src/Cena.js
@@ -1,22 +1,27 @@
 import React from 'react'
 
-function Cena(nombre, precio, color, setTotal) {
-      const comprar=()=>{
-            
+function Cena( {nombre, color, precio, setTotal} ) {
+      const comprar =(elemento)=>{
+            setTotal( (e)=> e+precio);
+            elemento.target.parentNode.parentNode.parentNode.style.display="none";
       }
-  return <>
-      <div className='rey'>
-            <h1>{nombre}</h1>
-            <div className='titulo'>Precio</div>
+      const ruta="https://www.html6.es/img/rey_"
+      const imagen = `${ruta}${nombre.toLowerCase()}.png`;
+      return (
+    <>
+      <div className='rey' style={ {backgroundColor:color} }>
+            <h2>{nombre}</h2>
+            <img src={imagen}/>
+            <div className='titulo'>Precio:</div>
             <div className='precio'>
                   {precio}$
                   <div>
                         <button onClick={comprar}>Comprar</button>
                   </div>
             </div>
-
       </div>
-    </>;
+    </>
+  )
 }
 
 export default Cena
